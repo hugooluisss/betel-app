52c4b37081162c695f53f2219370868a359ca8e4
Hugo Luis Santiago Altamirano <hugo.santiago@iebo.edu.mx>
2018-03-01 12:52:05 -0600

    Probando códigos

--- js/inicio.modulo.js
+++ js/inicio.modulo.js
@@ -39,7 +39,7 @@ var app = {
 			return false;
 		}, true);
 		console.log(applicationId);
-		alert("Hola mundo");
+
 		window.plugins.PushbotsPlugin.initialize(applicationId, {
 			"android":{
 				"sender_id": SenderID
